705e878fed749a05540247b45ccce46a04f06596
curtawhelan <curtawhelan@outlook.com>
2023-06-28 15:30:39 -0400

    added html semantics and meta tags

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Main() {
+  return (
+    <main>
+      <p>This is the main section. I like the main section its pretty cool</p>
+    </main>
+  )
+}
